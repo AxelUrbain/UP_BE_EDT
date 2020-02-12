d3eba0fb0bd197b185c7bc960644207b9b28fc69
URBAIN AXEL <aurbain@sfasp2mi.univ-poitiers.fr>
2020-02-12 11:00:16 +0100

    rename HomeController

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route(
+     *     {
+     *  "fr": "/",
+     *  "en": "/"
+     * }, name="login")
+     * @param Request $request
+     * @param AuthenticationUtils $authUtils
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(Request $request, AuthenticationUtils $authUtils)
+    {
+        $error = $authUtils->getLastAuthenticationError();
+
+        $lastUsername = $authUtils->getLastUsername();
+
+
+        return $this->render('home/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+    }
+
+    /**
+     * @Route({
+     *  "fr": "/login_check",
+     *  "en": "/login_check"
+     * }, name="login_check")
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function loginCheckAction()
+    {
+        throw new \Exception('Unexpexted loginCheck action');
+    }
+
+    /**
+     * @Route({
+     *  "fr": "/logout",
+     *  "en": "/logout"
+     * }, name="logout")
+     */
+    public function logout()
+    {
+        return $this->render('login.html.twig');
+    }
+}
